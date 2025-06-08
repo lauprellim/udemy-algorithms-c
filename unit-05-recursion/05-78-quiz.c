8f6521094ce4099a6bd6799753245c5f26bb0d35
Paul V. Miller <86619288+lauprellim@users.noreply.github.com>
2025-06-08 06:37:39 -0400

    Add files via upload

--- unit-05-recursion/05-78-quiz.c
+++ unit-05-recursion/05-78-quiz.c
@@ -29,6 +29,9 @@ int q4(int n) {
 
 void q5(int n) {
   // in the questions, this function is called count().
+  // this is much more straightforward than q4. remember
+  // the static variable does not change, so this returns
+  // 3 1 then 2 2 then 1 3 then 4 4 4
   static int d=1;
   printf("%d ", n);
   printf("%d ", d);
@@ -42,14 +45,35 @@ int main() {
   printf("Question 1: The value q1(1) returns is: %d\n\n", q1(1));
 
   // question 2
+  // This is an amusing thing, it will print "2 0 4 8" as the return values
+  // and sum will be 0 because that 0 belongs to MAIN function, not q2().
+  // sum is call by VALUE, it is not modified outside of q2()!
   int a = 2048, sum = 0;
   q2(a, sum);
   printf("Question 2: Sum is %d\n\n", sum);
 
   // question 3 has to be done in c++ because the function
   // is pass by reference using &.
-
+  // So I could mix C and C++ here but prefer to compile the different
+  // languages in different text files...
+  
   // question 4
+  // this one is pretty complex. Don't trace this as a tree. Rather
+  // make a table and start by tracing smaller values. It expands to:
+  // x + q4(1) * q4(4) + q4(2) * q4(3) + q4(3) * q4(2) + q4(4) * q4(1)
+  // so find value of q4(1) first, then make a table.
+  // when q4(1), return value is 1.
+  // when q4(2), return value is x + q4(1) * q4(2-1) so for loop repeats only once.
+  //      q4(2)  returns 1 + 1 * 1, which is 2.
+  // when q4(3), return value is x + q4(1) * q4(3-1) + q4(2) * q4(3-2)
+  //      q4(3)  returns 1 + 1 * 2 + 2 * 1
+  //             = 5
+  // when q4(4)  returns 1 + q4(1) * q4(3) + q4(2) * q4(2) + q4(3) * q4(1)
+  //             returns 1 + 1 * 5 + 2 * 2 + 5 * 1
+  //             = 15
+  // when q4(5)  returns 1 + 1 * 15 + 2 * 5 + 5 * 2 + 15 * 1
+  //             and the return value is 51.
+
   printf("Question 4: return value of q4(5) is: %d\n\n", q4(5));
   
   // question 5
--- unit-05-recursion/05-78-quiz.cpp
+++ unit-05-recursion/05-78-quiz.cpp
@@ -9,12 +9,13 @@ int q3(int &x, int c) {
 }
 
 int main() {
-  
   // question 3
+  // this will recurse until x = 9;
+  // but x is passed by reference, so when the chain of returns is triggered,
+  // it will be first 1 * 9, then 9 * 9, then 9^3, then 9^4
+  // (because we recursed four times);
+  // and the return value will be 9^4 which is 6561.
   int p = 5;
-  int *ptr;
-  ptr = &p;
-  // printf("p = %d and &p = %p\n", p, &p);
   printf("Question 3: return value of p is: %d\n", q3(p,p));
 
   return 0;
